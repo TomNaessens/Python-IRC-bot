63f943fb976b2dcaddc9b5a2f39a9a93fc7fb287
Tom Naessens <tomnaessens@yahoo.com>
2011-07-26 14:26:09 +0200

    Added the definitions file

---
+++ handlers/definitions.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.append("../")
+
+import mysql
+import settings
+
+def assign(conn, msg):
+    if len(msg.text.split()) > 2:
+        word = msg.text.split()[1]
+        defin = ' '.join(msg.text.split()[2:])
+        rowid = mysql.set('INSERT INTO irc_assign (word, def) VALUES (%s, %s)', (word, defin))
+        conn.send('PRIVMSG %s :%s added to assign list.\r\n' % (msg.channel, word))
+    else:
+        usage = 'Gebruik: !assign woord definitie'
+        conn.send('PRIVMSG %s :%s\r\n' % (msg.user, usage))
+
+def lijst(conn, msg):
+    aantal = 10
+    if len(msg.text.split()) > 1:
+        getal = msg.text.split()[1]
+        if getal.isdigit():
+            getal = int(getal)
+            if getal > 0:
+                aantal = getal
+            if getal > 30:
+                aantal = 30
+
+    rows, count = mysql.get('SELECT * FROM irc_assign ORDER BY id DESC', '')
+
+    if aantal > count:
+        aantal = count
+
+    out=[]
+    for row in range(0, aantal):
+        out.append(rows[row][1])
+
+    conn.send('PRIVMSG %s :Last %i assigns: %s\r\n' % (msg.channel, aantal, ', '.join(out)))
+
+def explain(conn, msg):
+     if len(msg.text.split()) > 1:
+         word = msg.text.split()[1]
+         rows, count = mysql.get('SELECT * FROM irc_assign WHERE word = %s', (word))
+         if count != 0:
+             conn.send('PRIVMSG %s :%s: %s\r\n' % (msg.channel, word, rows[0][2]))
+     else:
+        usage = 'Gebruik: ? woord'
+        conn.send('PRIVMSG %s :%s\r\n' % (msg.user, usage))
