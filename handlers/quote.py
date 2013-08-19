9f079b366f6e696c9d77ffaebeb2bcd8874ddbab
Tom Naessens <Tom.Naessens@UGent.be>
2013-08-19 11:55:35 +0200

    SQLite fixes

--- handlers/quote.py
+++ handlers/quote.py
@@ -8,7 +8,7 @@ import utils
 def addquote(conn, msg):
     if len(msg.text.split()) > 1:
         quote = ' '.join(msg.text.split()[1:])
-        rowid = sqlite.set('INSERT INTO irc_quote (quote) VALUES (%s)', (quote,))
+        rowid = sqlite.set('INSERT INTO irc_quote (quote) VALUES (?)', (quote,))
         conn.send('PRIVMSG %s :Quote %i added!\r\n' % (msg.channel, rowid))
     else:
         usage = 'Gebruik: !addquote quote'
@@ -17,7 +17,7 @@ def addquote(conn, msg):
 def delquote(conn, msg):
     if msg.user == settings.irc_OWNER or utils.isadmin(conn, msg):
         if len(msg.text.split()) > 1 and msg.text.split()[1].isdigit():
-            rowid = sqlite.set('DELETE FROM irc_quote WHERE id=%s', (msg.text.split()[1],))
+            rowid = sqlite.set('DELETE FROM irc_quote WHERE id=?', (msg.text.split()[1],))
             conn.send('PRIVMSG %s :Quote %i deleted!\r\n' % (msg.channel, int(msg.text.split()[1])))
         else:
             usage = 'Gebruik: !delquote id'
@@ -29,12 +29,12 @@ def delquote(conn, msg):
 def quote(conn, msg):
     parts = msg.text.split()
     if len(parts) == 1: # Random quote
-        rows, count = sqlite.get('SELECT * FROM irc_quote ORDER BY RAND()', ())
+        rows, count = sqlite.get('SELECT * FROM irc_quote ORDER BY RANDOM()', ())
 
     elif parts[1].isdigit(): # Haal quote nummer zoveel op
-        rows, count = sqlite.get('SELECT * FROM irc_quote WHERE id=%s', (parts[1],))
+        rows, count = sqlite.get('SELECT * FROM irc_quote WHERE id=?', (parts[1],))
 
     else:
-        rows, count = sqlite.get('SELECT * FROM irc_quote WHERE quote LIKE %s ORDER BY RANDOM()', ("%"+parts[1]+"%",))
+        rows, count = sqlite.get('SELECT * FROM irc_quote WHERE quote LIKE ? ORDER BY RANDOM()', ("%"+parts[1]+"%",))
     if count != 0:
         conn.send('PRIVMSG %s :Quote %i: %s\r\n' % (msg.channel, rows[0][0], rows[0][1]))
